b37d678ecc3386f29e82d1a6670cbfe41566ebf3
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-09-14 14:43:35 +0200

    Cambios en el renderizado de la imagen

--- pre/js/canvas-image.js
+++ pre/js/canvas-image.js
@@ -10,7 +10,7 @@ function setChartCanvasImage() {
     // Create a link
     var aDownloadLink = document.createElement('a');
     // Add the name of the file to the link
-    aDownloadLink.download = 'viz-maternidad-fecundidad.png';
+    aDownloadLink.download = 'mapa-alzheimer-viz.png';
     // Attach the data to the link
     aDownloadLink.href = image;
     // Get the code to click the download link
